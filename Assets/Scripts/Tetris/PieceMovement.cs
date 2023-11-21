865b5b9de222d23b8aa1e6a948af2197d2c98988
Cillian <cillsm80@gmail.com>
2023-11-21 13:40:12 +0000

    idk bitta tetris work

--- Assets/Scripts/Tetris/PieceMovement.cs
+++ Assets/Scripts/Tetris/PieceMovement.cs
@@ -1,10 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class PieceMovement : MonoBehaviour
 {
     private GameObject activePiece;
+
+    private Vector3 move;
+
+    private Quaternion rotation;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,36 @@ public class PieceMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("Swapped held piece");
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            move = activePiece.transform.position + Vector3.left;
+            activePiece.transform.SetPositionAndRotation(move, activePiece.transform.rotation);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("Slammed piece");
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            move = activePiece.transform.position + new Vector3(1,0,0);
+            activePiece.transform.SetPositionAndRotation(move, activePiece.transform.rotation);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            activePiece.transform.Rotate(0,0,45f);
+        }
         
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            activePiece.transform.Rotate(0,0,-45f);
+        }
     }
 }
